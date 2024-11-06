ef5db71308870207a93143029239885fb3a0dabf
Aku Sarma <akusarma1702@gmail.com>
2024-11-06 10:37:21 +0530

    done ;)

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/contact">contact</Link>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>
+        <span>A</span>
+        <span>K</span>
+        <span>U</span>
+        <span> </span>
+        <span>S</span>
+        <span>A</span>
+        <span>R</span>
+        <span>M</span>
+        <span>A</span>
+      </h1>
+      <p>Worlds No. 1 full stack developer</p>
+    </div>
+  );
+}
