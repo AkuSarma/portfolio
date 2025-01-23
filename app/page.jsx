489ae3c4fea0ba41bf18d7cc959dd4f361f567a5
Aku Sarma <akusarma1702@gmail.com>
2025-01-23 22:55:46 +0530

    add footer component, update layout and metadata, and remove unused files

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="home">
+        <section className="hero">
+          
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+}
